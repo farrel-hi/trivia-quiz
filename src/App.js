0f7b65b222fc6f637e25767e84a71fcee499e8c6
FarrelHI <farrel.indallah@gmail.com>
2023-09-26 21:32:18 +0700

    improve question component
    enable the difference display between two different question types

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import MainTitle from './component/MainTitle';
 import Card from './component/Card';
 import Login from './component/Login';
 import Question from './component/Question';
+import qData from './assets/question.json'
 
 function App() {
+  const data = qData.results;
   return (
     <div className="App">
       <Card>
@@ -15,7 +17,12 @@ function App() {
         <Login />
       </Card>
       <Card>
-        <Question />
+        <Question 
+          type={data[0].type}
+          question={data[0].question}
+          correct_answer={data[0].correct_answer}
+          incorrect_answers={data[0].incorrect_answers}
+        />
       </Card>
     </div>
   );
@@ -27,3 +34,7 @@ export default App;
 // https://bobbyhadz.com/blog/react-onclick-show-component
 // How to add Login/Auth to your React App:
 // https://www.youtube.com/watch?v=wr3VmbZdVA4
+// Open Trivia Database API:
+// https://opentdb.com/
+// 20 Question - General Knowledge - Multiple & True/False - Any Difficulty:
+// https://opentdb.com/api.php?amount=20&category=9
--- src/component/Question.js
+++ src/component/Question.js
@@ -1,19 +1,60 @@
 import React from "react";
 import './Question.css';
 
-function Question() {
-  const placeholder = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et dui ipsum. In vehicula dolor in ex ornare accumsan. Suspendisse.';
-  return(
-    <div className="question">
+function Question(props) {
+  const question = props.question;
+  const type = props.type;
+  const correct_answer = props.correct_answer;
+  const choices = [...props.incorrect_answers, correct_answer];
+
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
+
+  function MultipleChoice() {
+    shuffleArray(choices);
+    return (
+      <div className="multiple-container">
+        <button className="choices">{choices[0]}</button>
+        <button className="choices">{choices[1]}</button>
+        <button className="choices">{choices[2]}</button>
+        <button className="choices">{choices[3]}</button>
+      </div>
+    )
+  }
+
+  function BooleanChoice(){
+    return(
+      <div className="boolean-container">
+        <button className="choices">{choices[0]}</button>
+        <button className="choices">{choices[1]}</button>
+      </div>
+    );
+  }
+
+  function CheckType(typeProps){
+    if(typeProps.question_type == 'multiple'){
+      return <MultipleChoice />;
+    }
+    else{
+      return <BooleanChoice />;
+    }
+  }
+
+  return (
+    <div className="quiz">
       <div className="num-question">
         <h2>QUESTION</h2>
         <h2 className="number">0</h2>
       </div>
       <div className="question">
-        <h1>{placeholder}</h1>
+        <h1>{question}</h1>
       </div>
       <div className="answers">
-        
+        <CheckType question_type={type} />
       </div>
     </div>
   );
